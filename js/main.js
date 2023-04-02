05d0b7bbbcee87e25dd7ff7d581e285a7d023a13
Santiago Ramirez <santiagorave98@gmail.com>
2023-04-02 16:24:10 -0700

    First draft of the generateBoard method

--- js/main.js
+++ js/main.js
@@ -1,15 +1,58 @@
-const sudokuObject = function(difficulty){
-    this.difficulty=difficulty;
-    this.populateSudoku = function(difficulty) {
+const sudokuObject = function (difficulty) {
+    this.difficulty = difficulty;
+    this.populateNumbers = function (difficulty = "easy") {
+        // console.log(difficulty)
+        switch (difficulty) {
+            case "easy":
+
+                break;
+            case "medium":
+                break;
+            case "hard":
+                break;
+
+        }
     };
+    this.generateBoard = function() {
+        for(let i=0;i<9;i++) {
+        let rows = document.createElement("aside");
+        board.append(rows);   
+        }
+        let counter=0;
+
+        for( let aside of board.children ) {
+            for(let j=0;j<9;j++) {
+                let input = document.createElement("input");
+                input.value=++counter;
+                aside.append(input);
+
+            }  
+        }
+            // debugger;
+            // for(let j=0;j<9;j++) {
+            // let box = document.createElement("input");
+            // box.value=j;
+            // rows.append(box);
+
+            // }
+
+        
+
+    }
 
 }
+
+//easy 36   
+//medium 31
+//hard 25
 //DECLARATIONS
 let levelButtons = document.getElementsByClassName("buttons")[0];
-const generateButtons = function() {
-    for(let i=0;i<3;i++){
+let board = document.getElementById("sudoku");
+
+const generateButtons = function () {
+    for (let i = 0; i < 3; i++) {
         let button = document.createElement("button");
-        button.id=i;
+        button.id = i;
         button.classList.add("hover");
         levelButtons.append(button);
     }
@@ -19,5 +62,6 @@ const generateButtons = function() {
 
 };
 generateButtons();
-
+let sudoku = new sudokuObject();
+sudoku.generateBoard();
 
