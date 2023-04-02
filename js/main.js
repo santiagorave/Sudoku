7aaa60f432e529442429d4fbe89b238e3906e323
Dylan5023 <kangjun5023@gmail.com>
2023-04-02 15:47:11 -0700

    Create CSS,Hover and button id

--- js/main.js
+++ js/main.js
@@ -10,10 +10,14 @@ const generateButtons = function() {
     for(let i=0;i<3;i++){
         let button = document.createElement("button");
         button.id=i;
+        button.classList.add("hover");
         levelButtons.append(button);
-
     }
-}
+    document.getElementById('0').innerText = 'Easy';
+    document.getElementById('1').innerText = 'Medium';
+    document.getElementById('2').innerText = 'Hard';
+
+};
 generateButtons();
 
 
