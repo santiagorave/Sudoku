85debe171d929661ce9edf88ab57ee11b82683e9
Santiago Ramirez <santiagorave98@gmail.com>
2023-04-09 12:11:40 -0700

    CSS Fixed

--- js/main.js
+++ js/main.js
@@ -1,110 +1,115 @@
-// const sudokuObject = function (difficulty) {
-//     this.difficulty = difficulty;
-//     this.generateSolution = function (difficulty = "easy") {
-//         let array2d=[];
-//         let inputs = document.getElementsByTagName("input");
-//         // console.log(inputs[0].value);
-//         for(let k=0;k<9;k++){
-//         array2d.push(this.randomValues());
-//         }
-//         console.log(array2d);
-//         let counter=0;
-//         let arrray= [];
+const sudokuObject = function (difficulty) {
+    this.difficulty = difficulty;
+    this.generateSolution = function (difficulty = "easy") {
+        let array2d=[];
+        let inputs = document.getElementsByTagName("input");
+        // console.log(inputs[0].value);
+        for(let k=0;k<9;k++){
+        array2d.push(this.randomValues());
+        }
+        console.log(array2d);
+        let counter=0;
+        let arrray= [];
 
-//         for(let l=0;l<9;l++){
-//         //     console.log(array2d[l][1]);
-//         // arrray.push(array2d[l][0]);
-//         for(let m=0;m<9;m++){
-//         inputs[counter++].value=array2d[l][m]; //Pushing to html
-//         console.log(array2d[l][m]);
-//             // console.log(`L: ${l}    M: ${m}      counter:${counter}  input:${inputs[counter].value}`)
-//         }
+        for(let l=0;l<9;l++){
+        //     console.log(array2d[l][1]);
+        // arrray.push(array2d[l][0]);
+        for(let m=0;m<9;m++){
+        inputs[counter++].value=array2d[l][m]; //Pushing to html
+        console.log(array2d[l][m]);
+            // console.log(`L: ${l}    M: ${m}      counter:${counter}  input:${inputs[counter].value}`)
+        }
 
-//         }
-//         console.log(arrray);
-
-
-
-
-//         // switch (difficulty) {
-//         //     case "easy":
-//         //         break;
-//         //     case "medium":
-//         //         break;
-//         //     case "hard":
-//         //         break;
-
-//         // }
-//     };
-//     this.randomValues = function() {
-//             let arr = [];
-//             let idx = 0;
-//             while (idx < 9) {
-//             let num = Math.floor(Math.random() * 9) + 1;
-//             if (!checkNum(num)) {
-//             arr.push(num);
-//             idx++;
-//             }
-//             };
-
-//             function checkNum(num) {
-//             for (let i = 0; i < arr.length; i++) {
-//             if (num === arr[i]) {
-//             return  true;
-//             }
-//             }
-//             return false;
-//             }
-//             return arr; 
-//     };
-//     this.generateBoard = function() {
-//         for(let i=0;i<9;i++) {
-//         let rows = document.createElement("aside");
-//         board.append(rows);   
-//         }
-//         let counter=0;
-
-//         for( let aside of board.children ) {
-//             for(let j=0;j<9;j++) {
-//                 let input = document.createElement("input");
-//                 input.id=++counter;
-//                 aside.append(input);
-
-//             }  
-//         }
+        }
+        console.log(arrray);
 
 
 
-//     }
 
-// }
+        // switch (difficulty) {
+        //     case "easy":
+        //         break;
+        //     case "medium":
+        //         break;
+        //     case "hard":
+        //         break;
+
+        // }
+    };
+    this.randomValues = function() {
+            let arr = [];
+            let idx = 0;
+            while (idx < 9) {
+            let num = Math.floor(Math.random() * 9) + 1;
+            if (!checkNum(num)) {
+            arr.push(num);
+            idx++;
+            }
+            };
+
+            function checkNum(num) {
+            for (let i = 0; i < arr.length; i++) {
+            if (num === arr[i]) {
+            return  true;
+            }
+            }
+            return false;
+            }
+            return arr; 
+    };
+    this.generateBoard = function() {
+        for(let i=0;i<9;i++) {
+        let rows = document.createElement("aside");
+        board.append(rows);   
+        }
+        let counter=0;
+
+        for( let aside of board.children ) {
+            for(let j=0;j<9;j++) {
+                let input = document.createElement("input");
+                input.id=++counter;
+                aside.append(input);
+
+            }  
+        }
+
+
+
+    }
+
+}
 
 // //easy 36   
 // //medium 31
 // //hard 25
 // //DECLARATIONS
-// let levelButtons = document.getElementsByClassName("buttons")[0];
-// let board = document.getElementById("sudoku");
+let levelButtons = document.getElementsByClassName("buttons")[0];
+let board = document.getElementById("sudoku");
 
-// const generateButtons = function () {
-//     for (let i = 0; i < 3; i++) {
-//         let button = document.createElement("button");
-//         button.id = `button-${i}`;
-//         button.classList.add("hover");
-//         levelButtons.append(button);
-//     }
-//     document.getElementById('button-0').innerText = 'Easy';
-//     document.getElementById('button-1').innerText = 'Medium';
-//     document.getElementById('button-2').innerText = 'Hard';
+const generateButtons = function () {
+    for (let i = 0; i < 3; i++) {
+        let button = document.createElement("button");
+        button.id = `button-${i}`;
+        button.classList.add("hover");
+        levelButtons.append(button);
+    }
+    document.getElementById('button-0').innerText = 'Easy';
+    document.getElementById('button-1').innerText = 'Medium';
+    document.getElementById('button-2').innerText = 'Hard';
 
-// };
-// generateButtons();
+};
+generateButtons();
 
-// let sudoku = new sudokuObject();
-// sudoku.generateBoard();
-// sudoku.generateSolution();
-// let arrayTest = [[], [], [], [], [], [], [], [], []];
-// let counter = 0;
+let sudoku = new sudokuObject();
+sudoku.generateBoard();
+sudoku.generateSolution();
+let arrayTest = [[], [], [], [], [], [], [], [], []];
+let counter = 0;
+
+const getJSON = () => {
+    const request = new XMLHttpRequest();
+}
+getJSON();
 
 // for (let row = 0; row < 9; row++) {
 //     for (let column = 0; column < 9; column++) {
